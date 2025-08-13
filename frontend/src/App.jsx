af03465bd519b5595fd6f8756628075138388a8d
sinjwu <harbirdouk@gmail.com>
2025-08-13 15:34:06 +0900

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { todoApi } from "./services/api";
+
 const App = () => {
+  useEffect(() => {
+    const test = async () => {
+      const data = await todoApi.getAllTodos();
+      console.log(data);
+    };
+    test();
+  }, []);
   return <div className="bg-orange-100">App</div>;
 };
 export default App;
