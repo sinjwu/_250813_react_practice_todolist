99930e2f831e70534541f124abf4965cd62a8ceb
sinjwu <harbirdouk@gmail.com>
2025-08-13 10:45:30 +0900

    first commit

---
+++ backend/src/test/java/com/example/_250813_react_practice_todolist_backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._250813_react_practice_todolist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <div className="bg-orange-100">App</div>;
+};
+export default App;
